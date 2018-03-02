fc9dea91d8840538f570a482d4bbe74001b4e252
Abhijit Hycinth <abhijeethycinth@gmail.com>
2018-03-02 20:01:52 +0530

    Redirect from create posts added

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,2 +1,7 @@
 @extends('main')
+@section('title','View Post')
 @section('image',asset('img/contact-bg.jpg'))
+@section('content')
+  <h1><small>{{$post->title}}</small></h1>
+  <p>{{$post->body}}</p>
+@endsection
