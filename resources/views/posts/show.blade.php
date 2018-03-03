38dcdfe52e01fc27b59234a60760e8cbf60bb1b7
Abhijit Hycinth <abhijeethycinth@gmail.com>
2018-03-04 02:12:53 +0530

    Updated create posts page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,42 @@
 @section('title','View Post')
 @section('image',asset('img/contact-bg.jpg'))
 @section('content')
-  <h1><small>{{$post->title}}</small></h1>
-  <p>{{$post->body}}</p>
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+    <div class="well">
+      <div class="row">
+        <div class="col-md-6">
+          <dl>
+            <dt><u>{{$post->title}}</u></dt>
+            <dd class="lead"><small>{{$post->body}}</small></dd>
+          </dl>
+        </div>
+        <div class="col-md-6">
+              <dl class="dl-horizontal">
+                <dt><small>Created At:</small></dt>
+                <dd>
+                  <small>{{date("M d Y h:i:A",strtotime($post->created_at))}}</small>
+                </dd>
+              </dl>
+              <dl class="dl-horizontal">
+                <dt><small>Last Updated At:</small></dt>
+                <dd>
+                  <small>{{date("M d Y h:i:A",strtotime($post->updated_at))}}</small>
+                </dd>
+              </dl>
+              <div class="row">
+                <center>
+                  <div class="col-md-6">
+                  {!! Html::linkRoute('posts.edit','Edit',array($post->id),array('class'=>'btn btn-primary btn-block'))!!}
+                  </div>
+                  <div class="col-md-6">
+                  {!! Html::linkRoute('posts.destroy','Delete',array($post->id),array('class'=>'btn btn-danger btn-block'))!!}
+                  </div>
+                </center>
+              </div>
+        </div>
+      </div>
+  </div>
+  </div>
+</div>
 @endsection
